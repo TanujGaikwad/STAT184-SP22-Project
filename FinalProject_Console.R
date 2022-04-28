2430b11c25d0eb6b4f7852dff3ae29d13f93f40b
TanujGaikwad <tanuj.gaikwad@gmail.com>
2022-04-28 00:37:37 -0400

    Work for 04/27/2022: Second and Third Chunks

--- FinalProject_Console.R
+++ FinalProject_Console.R
@@ -1,4 +1,4 @@
-# LOad 2 packages & read in data
+# Load 2 packages & read in data
 library(tidyverse, warn.conflicts = FALSE)
 library(lubridate)
 
@@ -21,9 +21,18 @@ head(Ratings, 3)
 head(Tags, 3)
 
 # Second Chunk: Joining data where Person RATED & TAGGED a movie
-Newtable <-
-  Ratings %>%
-  full_join(Tags, by = "userId")
+Ratings_n_Tags <-
+  Tags %>%
+  left_join(Ratings, by = c("movieId", "userId")) %>%
+  left_join(Movies, by = "movieId") %>%
+  na.omit() %>%
+  select(userId, rating, rating_date, tag, tag_date, title, genres)
 
-NewTable <-
-  select(movieId, userId, rating, rating_date, tag, tag_date)
+head(Ratings_n_Tags, 3)
+
+# Third Chunk: New release_yeah column and modifying movie titles
+Ratings_n_Tags <-
+  Ratings_n_Tags %>%
+  mutate(release_year = str_extract(title, "\\d+"))
+
+Ratings_n_Tags$title <- gsub("\\(\\d+)", "", Ratings_n_Tags$title)
